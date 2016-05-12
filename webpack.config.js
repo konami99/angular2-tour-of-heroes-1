d3883ff5302ca310fe33b924e3f2ba761dc0234d
Richard Chou <konami99@hotmail.com>
2016-05-12 11:44:33 +1000

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,20 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+
+const ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+
+const metadata = {
+    env: ENV,
+    host: HOST,
+    port: PORT
+}
+
 module.exports = {
+    debug: true,
     entry: {
         'main': './app/main.ts',
         'vendor': './app/vendor.ts'
@@ -12,7 +25,8 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
-        new CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+        new CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+        new DefinePlugin({'webpack': {'ENV': JSON.stringify(metadata.env)}})
         //new CopyWebpackPlugin([{from: './index.html', to: 'index.html'}])
     ],
     resolve: {
@@ -20,16 +34,18 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.css$/,  loader: 'raw', exclude: /node_modules/},
-            {test: /\.css$/,  loader: 'style!css?-minimize', exclude: /app/},
+            {test: /\.css$/, loader: 'raw', exclude: /node_modules/},
+            {test: /\.css$/, loader: 'style!css?-minimize', exclude: /app/},
             {test: /\.html$/, loader: 'raw'},
-            {test: /\.ts$/,   loader: 'ts', query: {compilerOptions: {noEmit: false}}}
+            {test: /\.ts$/, loader: 'ts', query: {compilerOptions: {noEmit: false}}}
         ],
         noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
     },
     devServer: {
         contentBase: 'app',
-        historyApiFallback: true
+        historyApiFallback: true,
+        host: metadata.host,
+        port: metadata.port
     },
     devtool: 'source-map'
 }
