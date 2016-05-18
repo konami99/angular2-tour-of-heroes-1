243585b9eef2926e8b2975712488372813c755b4
Richard Chou <konami99@hotmail.com>
2016-05-18 21:30:08 +0530

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,14 +15,14 @@ set :deploy_to, '/usr/local/angular'
 # set :scm, :git
 
 # Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :format, :pretty
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
