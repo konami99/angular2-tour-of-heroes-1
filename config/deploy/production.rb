10c7da8db0079010a56bd153d3e6feccb86b9936
Richard Chou <konami99@hotmail.com>
2016-05-18 21:49:32 +0530

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server 'ec2-54-186-77-113.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
@@ -41,11 +41,11 @@ role :db,  %w{deploy@ec2-54-186-77-113.us-west-2.compute.amazonaws.com}
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+    keys: %w(/home/rlisowski/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(password)
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
