a237d084e6f7329b0ae8df439732de7b02952611
Richard Chou <konami99@hotmail.com>
2016-05-18 22:36:19 +0530

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ server 'ec2-54-186-77-113.us-west-2.compute.amazonaws.com', user: 'deploy', role
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+set :branch, 'webpack'
 
 # role-based syntax
 # ==================
