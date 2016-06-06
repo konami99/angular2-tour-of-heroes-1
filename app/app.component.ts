59a2e383349a7b5e57f0caa6cdceb793d12c610f
Richard Chou <konami99@hotmail.com>
2016-06-06 11:26:21 +1000

    webpack

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,8 +20,8 @@ import { HeroService } from './hero.service';
   directives: [ROUTER_DIRECTIVES],
   providers: [
     HeroService,
-    ROUTER_PROVIDERS
-    
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
   ]
 })
 @RouteConfig([
